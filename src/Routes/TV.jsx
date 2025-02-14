6acd12b6f0a026ef7f03c9ee0272cbd4ea4e1dcd
HyunJongLee <dlguswhd1009@naver.com>
2025-02-15 00:11:14 +0900

    update layout

--- src/Routes/TV.jsx
+++ src/Routes/TV.jsx
@@ -35,12 +35,15 @@ export default function Tv() {
   const [clickInfo, setClickInfo] = useState(false);
 
   return (
-    <Wrapper>
+    <Wrapper isDesktop={isDesktop}>
       {isLoading ? (
         <Loader>isLoading..</Loader>
       ) : (
         <>
-          <Banner bgphoto={makeImgPath(results?.[0].backdrop_path || "")}>
+          <Banner
+            bgphoto={makeImgPath(results?.[0].backdrop_path || "")}
+            isDesktop={isDesktop}
+          >
             <BannerInfo isDesktop={isDesktop}>
               <Title isDesktop={isDesktop}>{results?.[0].name}</Title>
               <Overview isDesktop={isDesktop}>{results?.[0].overview}</Overview>
